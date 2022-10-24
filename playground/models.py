81ed9b1396d8ed09d2c83d14fa63ec877704a072
dominikJakov <21IMC10051@fh-krems.ac.at>
2022-10-24 20:54:13 +0200

    testing pushing

--- playground/models.py
+++ playground/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
+    # testinig the code
     name = models.CharField("Even Name" , max_length=120)
 
     def __str__(self):
